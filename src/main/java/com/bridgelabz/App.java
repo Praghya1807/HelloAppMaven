747e5e5f262904543ebcca083197611bf2fc2dca
Praghya <pg6408@srmist.edu.in>
2026-04-10 09:29:44 +0530

    Coded UC7: Display Hello with multiple args using String.join()

--- src/main/java/com/bridgelabz/App.java
+++ src/main/java/com/bridgelabz/App.java
@@ -7,19 +7,12 @@ public class HelloApp {
         // If no arguments
         if (args.length == 0) {
             System.out.println("Hello, World!");
-            return;
+        }
+        else {
+            // Join all names with comma
+            String names = String.join(", ", args);
+            System.out.println("Hello, " + names + "!");
         }
 
-        StringBuilder nameBuilder = new StringBuilder();
-
-        // Enhanced for loop
-        for (String name : args) {
-            nameBuilder.append(name).append(", ");
-        }
-
-        // Remove last comma and space
-        String names = nameBuilder.substring(0, nameBuilder.length() - 2);
-
-        System.out.println("Hello, " + names + "!");
     }
 }
